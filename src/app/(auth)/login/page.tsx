4a690befd63a464e63d1d466b49f5bdd1f6b5ee0
= <abubakaribilal99@gmail.com>
2024-12-20 10:38:45 +0000

    refactor: convert Login to function declaration

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,10 +2,12 @@ import { LoginSlide } from '@/assets/images';
 import AuthenticationFrame from '@/app/(auth)/_components/authenticationFrame';
 import { LoginForm } from '@/app/(auth)/_components/loginForm';
 
-export default function Login() {
+function Login() {
   return (
     <AuthenticationFrame imageSlide={LoginSlide} imageAlt="Login">
       <LoginForm />
     </AuthenticationFrame>
   );
 }
+
+export default Login;
