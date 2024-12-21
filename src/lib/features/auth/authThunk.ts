6006735590166dbc6ee360541aab315d711e87d5
= <abubakaribilal99@gmail.com>
2024-12-21 21:21:43 +0000

    feat: add login request and logic

--- src/lib/features/auth/authThunk.ts
+++ src/lib/features/auth/authThunk.ts
@@ -1,14 +1,14 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ILogin } from '@/app/(auth)/_components/loginForm';
-import axios, { axiosErrorHandler } from '@/lib/axios';
-import { setErrorMessage } from '@/lib/features/auth/authSlice';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ILogin } from "@/app/(auth)/_components/loginForm";
+import axios, { axiosErrorHandler } from "@/lib/axios";
+import { setErrorMessage } from "@/lib/features/auth/authSlice";
 
 export const login = createAsyncThunk(
-  'authentication/login',
+  "authentication/login",
   async (loginCredentials: ILogin, { dispatch }) => {
     try {
-      const { data } = await axios.post('auth/login', loginCredentials);
-      console.log('Data'); // TODO: Backend needs to adjust before we can handle this
+      const { data } = await axios.post("auth/login", loginCredentials);
+      console.log("Data", data); // TODO: Backend needs to adjust before we can handle this
       return true;
     } catch (error) {
       dispatch(setErrorMessage(axiosErrorHandler(error)));
