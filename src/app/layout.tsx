646099a99239aa17264e6f4d3b478dc8b6cd96fe
= <abubakaribilal99@gmail.com>
2024-12-21 20:15:44 +0000

    feat: configure redux store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import StoreProvider from "@/app/storeProvider";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Zomujo',
+  title: "Zomujo",
   description:
-    'A secure healthcare platform to boost patient engagement and improve overall healthcare delivery',
+    "A secure healthcare platform to boost patient engagement and improve overall healthcare delivery",
 };
 
 export default function RootLayout({
@@ -25,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
     </html>
   );
 }
