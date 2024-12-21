e4a66ca9ec4b05ea3de3f2a6d66ac069ccba6651
= <abubakaribilal99@gmail.com>
2024-12-21 20:58:21 +0000

    feat: implement custom axios and error handler

---
+++ src/lib/axios.ts
@@ -0,0 +1,18 @@
+import axios, { isAxiosError } from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const networkFailureErrorMessage = 'Oops! Server Error... Please check your internet connection';
+
+export default axios.create({
+  baseURL: BASE_URL,
+});
+
+export const axiosErrorHandler = (error: unknown): string => {
+  if (isAxiosError(error)) {
+    return error.response?.data.message ?? networkFailureErrorMessage;
+  } else if (error instanceof Error) {
+    return error.message;
+  }
+  return networkFailureErrorMessage;
+};
