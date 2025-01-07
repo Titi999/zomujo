3817f3e0d0be39e66bf2329f7ac78593f89675c8
= <abubakaribilal99@gmail.com>
2025-01-07 16:40:14 +0000

    fix: update extra after doctor onboarding

--- src/app/dashboard/_components/dashboardProvider.tsx
+++ src/app/dashboard/_components/dashboardProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { selectUser } from '@/lib/features/auth/authSelector';
+import { selectExtra, selectUser } from '@/lib/features/auth/authSelector';
 import { useAppSelector } from '@/lib/hooks';
 import { useRouter } from 'next/navigation';
 import { JSX, useEffect, FC } from 'react';
@@ -9,15 +9,17 @@ import { Role } from '@/types/shared.enum';
 function dashboardProvider(Component: FC) {
   return function DashboardProvider(props: JSX.IntrinsicAttributes) {
     const user = useAppSelector(selectUser);
+    const extra = useAppSelector(selectExtra);
     const router = useRouter();
+
     useEffect(() => {
       if (!user) {
         router.push('/login');
       }
-      if (user?.role === Role.Doctor && !user?.extra) {
+      if (user?.role === Role.Doctor && !extra) {
         router.push('/onboarding');
       }
-    }, [user]);
+    }, [user, extra]);
     return <Component {...props} />;
   };
 }
